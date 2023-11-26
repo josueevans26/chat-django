daf862f5f59ac5604c8455f3e49a1026780e31cd
Joshua Evans <jevangelistag@toluca.tecnm.mx>
2023-11-25 23:02:48 -0600

    Cambio en db2

--- app/settings.py
+++ app/settings.py
@@ -78,6 +78,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+CERTIFICATE_PATH = os.path.join(BASE_DIR, '..', 'DigiCertGlobalRootCA.crt.pem')
+
 DATABASES = {
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
@@ -91,7 +96,7 @@ DATABASES = {
         'HOST': 'db-chatdjango.mysql.database.azure.com',
         'PORT': '3306',
         'OPTIONS': {
-            'ssl': {'ca': '../DigiCertGlobalRootCA.crt.pem'},
+            'ssl': {'ca': CERTIFICATE_PATH},
         },
     }
 }
