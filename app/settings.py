9bd0e983473981cf53c4c68396340a29be206bd0
Joshua Evans <jevangelistag@toluca.tecnm.mx>
2023-11-25 20:40:48 -0600

    Se ALLOWED_HOSTS en settings

--- app/settings.py
+++ app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-xjuh0d=5@epef@1@6)-_$6o*5nvhgy3osd=66ck9sfb20+6jw^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['chatdjango.azurewebsites.net']
 
 
 # Application definition
