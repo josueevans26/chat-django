bd7e3c7e8a91bfb88f3ba606c06a30779378f1a5
Joshua Evans <jevangelistag@toluca.tecnm.mx>
2023-11-27 09:55:36 -0600

    Cambio de ruta ssl

--- app/settings.py
+++ app/settings.py
@@ -78,11 +78,6 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-CERTIFICATE_PATH = os.path.join(BASE_DIR, '..', 'DigiCertGlobalRootCA.crt.pem')
-
 DATABASES = {
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
@@ -96,7 +91,7 @@ DATABASES = {
         'HOST': 'db-chatdjango.mysql.database.azure.com',
         'PORT': '3306',
         'OPTIONS': {
-            'ssl': {'ca': CERTIFICATE_PATH},
+            'ssl': {'ca': 'DigiCertGlobalRootCA.crt.pem'},
         },
     }
 }
